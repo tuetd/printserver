b7350950dce9f5ec984e76a651845b0c9e3e196a
tuetd <trandinhtue1987@gmail.com>
2021-02-24 15:25:36 +0700

    comit;

--- src/com/pruvn/rms/utils/CSRecordDecorator.java
+++ src/com/pruvn/rms/utils/CSRecordDecorator.java
@@ -2,6 +2,7 @@ package com.pruvn.rms.utils;
 
 import org.displaytag.decorator.TableDecorator;
 
+@SuppressWarnings("unchecked")
 public class CSRecordDecorator extends TableDecorator{
 	
     public String addRowId()
